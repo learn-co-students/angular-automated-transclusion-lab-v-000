08ca5b921f618f4a36b06143ef28bb7bf05013f3
Brian Holland <beehollander@gmail.com>
2016-05-20 17:31:13 -0500

    Done.

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,13 +1,15 @@
 function UserProfile() {
-	return {
-		template: [
-			'<div>',
-				'<h3>User Profile</h3>',
-			'</div>'
-		].join('')
-	}
+  return {
+    transclude: true,
+    template: [
+      '<div>',
+      '<h3>User Profile</h3>',
+      '<div ng-transclude></div>',
+      '</div>'
+    ].join('')
+  }
 }
 
 angular
-	.module('app')
-	.directive('userProfile', UserProfile);
+.module('app')
+.directive('userProfile', UserProfile);
