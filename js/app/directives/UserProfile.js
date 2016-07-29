33fa997a151ed9ad26cdcce0a190c174e6c2a668
tejbans <singh.tejbans@gmail.com>
2016-07-29 15:40:11 -0700

    Done.

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,8 +1,11 @@
 function UserProfile() {
 	return {
+    transclude: true,
 		template: [
 			'<div>',
 				'<h3>User Profile</h3>',
+        '<h4 ng-transclude></h4>',
+        '<h6 ng-transclude></h6>',
 			'</div>'
 		].join('')
 	}
