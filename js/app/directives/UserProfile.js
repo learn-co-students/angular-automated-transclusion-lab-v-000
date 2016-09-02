f79deb27c3a684e55559fa02fa4c7eadccf8af14
agdavid <antonio.g.david@gmail.com>
2016-09-02 14:58:13 -0400

    use ng-transclude to insert existing HTML into directive template

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,8 +1,12 @@
 function UserProfile() {
 	return {
-		template: [
+		transclude: true,
+    template: [
+      //at each <user-profile> element this template is inserted
 			'<div>',
 				'<h3>User Profile</h3>',
+        //use ng-transclude to insert the existing HTML
+        '<span ng-transclude></span>',
 			'</div>'
 		].join('')
 	}
