76ea28441241fe760461222a6d38ac3350d7a58d
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-10-13 15:32:16 -0400

    Done.

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,8 +1,10 @@
 function UserProfile() {
 	return {
+    transclude: true,
 		template: [
 			'<div>',
 				'<h3>User Profile</h3>',
+        '<span ng-transclude></span>',
 			'</div>'
 		].join('')
 	}
@@ -11,3 +13,11 @@ function UserProfile() {
 angular
 	.module('app')
 	.directive('userProfile', UserProfile);
+
+
+// In this repo we have a UserProfile 
+// directive, and we're using it in index.html. 
+// However, our user info is getting overridden 
+// by the directive's template.
+// Using your knowledge of transclusion, make our 
+// information display properly in the directive!
