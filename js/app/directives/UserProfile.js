94691dc5bd84e8d962b1d4ccaf3fcca7563ec14d
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-10-13 15:32:34 -0400

    Done.

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -13,11 +13,3 @@ function UserProfile() {
 angular
 	.module('app')
 	.directive('userProfile', UserProfile);
-
-
-// In this repo we have a UserProfile 
-// directive, and we're using it in index.html. 
-// However, our user info is getting overridden 
-// by the directive's template.
-// Using your knowledge of transclusion, make our 
-// information display properly in the directive!
