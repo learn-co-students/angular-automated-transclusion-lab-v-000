6a4fc04672d0501e030bec74812923faf0755487
Steve Frost <ffrostt@gmail.com>
2016-12-12 22:59:15 -0500

    set transclude: true & used ng-transclude in UserProfile directive

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,11 +1,13 @@
 function UserProfile() {
 	return {
+    transclude: true,
 		template: [
 			'<div>',
 				'<h3>User Profile</h3>',
+        '<span ng-transclude></span>',
 			'</div>'
 		].join('')
-	}
+	};
 }
 
 angular
